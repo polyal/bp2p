e11b4fc31cd62c564441bd8060b2a53776cc9cd7
Lorant Polya <lorant.polya@gmail.com>
2019-10-24 03:37:11 -0400

    made device discovery helper functions private

--- src/btdevice.h
+++ src/btdevice.h
@@ -27,13 +27,15 @@ public:
 	int endServerComm();
 
 	int findNearbyDevs(vector<DeviceDescriptor>& devs);
-	int getInqInfo(inquiry_info*& inqInf, int& numDevs);
-	int inqInfList2DevDesList(vector<DeviceDescriptor>& devs, const inquiry_info* const inqInf, int numDevs);
-	int inqInf2DevDes(DeviceDescriptor& dev, const inquiry_info& inqInf);
 
 	static int findLocalDevs(vector<DeviceDescriptor>& devs);
 	static int getHCIDevList(struct hci_dev_list_req*& devList, int& numDevs);
 	static int HCIDevList2DevDesList(vector<DeviceDescriptor>& devs,
 									 const struct hci_dev_list_req* const devList, int nDevs);
 	static bool HCIDev2DevDes(DeviceDescriptor& dev, const struct hci_dev_req& devReq);
+
+private:
+	int getInqInfo(inquiry_info*& inqInf, int& numDevs);
+	int inqInfList2DevDesList(vector<DeviceDescriptor>& devs, const inquiry_info* const inqInf, int numDevs);
+	int inqInf2DevDes(DeviceDescriptor& dev, const inquiry_info& inqInf);
 };
