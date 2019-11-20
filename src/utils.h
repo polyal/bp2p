5b02bca36a7e59232e1b424628202ab1ec80822b
Lorant Polya <lorant.polya@gmail.com>
2019-11-20 01:00:00 -0500

    added filterDuplicates function and cleaned up alignment

--- src/utils.h
+++ src/utils.h
@@ -4,33 +4,37 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <unordered_set>
 
 using namespace std;
 
 class Utils{
-	public:
-		// application type utils
-		inline static const string applicationDir = "/bp2p/";
-		static string getHomeDir();
-		static string getApplicationPath();
+public:
+	// application type utils
+	inline static const string applicationDir = "/bp2p/";
+	static string getHomeDir();
+	static string getApplicationPath();
 
-		// string utils
-		static void tokenize(const string& text, const string& sep, vector<string>& tokens);
+	// string utils
+	static void tokenize(const string& text, const string& sep, vector<string>& tokens);
 
-		// file utils
-		static bool doesFileExist(const string& filename);
-		static void converToFullpath(const string& relPath, string& fullPath);
-		static int isRegulaFile(const string& path);
-		static int listFileInDir(const string& dirName, vector<string>& filenames);
-		static unsigned long long filesize(const char* filename);
+	// file utils
+	static bool doesFileExist(const string& filename);
+	static void converToFullpath(const string& relPath, string& fullPath);
+	static int isRegulaFile(const string& path);
+	static int listFileInDir(const string& dirName, vector<string>& filenames);
+	static unsigned long long filesize(const char* filename);
 
-		// hex and byte utils
-		static string bytesToHex(char* bytes, int len);
-		static unsigned int value(char c);
-		static char* hexToBytes(const string& strhex, int* size);
+	// hex and byte utils
+	static string bytesToHex(char* bytes, int len);
+	static unsigned int value(char c);
+	static char* hexToBytes(const string& strhex, int* size);
 
-		// random numbers
-		static int grnd(int min, int max);
+	// random numbers
+	static int grnd(int min, int max);
+
+	template <class T>
+	unordered_set<T> filterDuplicates(const vector<T> list);
 };
 
 #endif
