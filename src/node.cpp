255c42b741b0827e56597bcd8bfbf72c6f4b47bc
Lorant Polya <lorant.polya@gmail.com>
2019-11-28 20:57:58 -0500

    another torrentFileReq bug fix

--- src/node.cpp
+++ src/node.cpp
@@ -92,8 +92,8 @@ void Node::sendRequestWait4Response(const Message& req, Message& rsp,
 	catch(int e){
 		cout << "Caught Exception " << e << endl;
 	}
-	//string strresp{rsp.data.begin(), rsp.data.end()};
-	//cout << "CLIENT RSP: " << rsp.data.size() << " " << rsp.size << endl;
+	string strresp{rsp.data.begin(), rsp.data.end()};
+	cout << "CLIENT RSP: " << rsp.data.size() << " " << rsp.size << " " << strresp << endl;
 	try{
 		client.endComm();
 	}
@@ -147,7 +147,8 @@ void Node::getSerializedTorrent(string& serializedTorrent, const string& name)
 	auto nameTorPair = this->name2torrent.find(name);
 	if (nameTorPair != this->name2torrent.end()){
 		Torrent tor = nameTorPair->second;
-		serializedTorrent = tor.getSerializedTorrent();
+		if (tor.open())
+			serializedTorrent = tor.getSerializedTorrent();
 	}
 }
 
