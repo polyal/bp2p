cfc77e3f5e6d3bf6cf3bdc885fe86f80e2392502
Lorant Polya <lorant.polya@gmail.com>
2020-01-04 23:05:25 -0500

    started working on DatabaseConnector class

--- src/db.cpp
+++ src/db.cpp
@@ -1,25 +1,141 @@
-/* Standard C++ includes */
-#include <stdlib.h>
-#include <iostream>
+#include "db.h"
 
-/*
-  Include directly the different
-  headers from cppconn/ and mysql_driver.h + mysql_util.h
-  (and mysql_connection.h). This will reduce your build time!
-*/
-#include "mysql_connection.h"
 
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
+string DatabaseConnector::tcp = "tcp://";
 
-using namespace std;
+DatabaseConnector::DatabaseConnector()
+{	
+}
+
+DatabaseConnector::DatabaseConnector(const string& ip, const string& port, const string& user, const string& pwd)
+{
+	this->ip = ip;
+	this->port = port;
+	this->user = user;
+	this->pwd = pwd;
+}
+
+DatabaseConnector::DatabaseConnector(const string& ip, const string& port, const string& user, const string& pwd, 
+	const string& schema)
+{
+	this->ip = ip;
+	this->port = port;
+	this->user = user;
+	this->pwd = pwd;
+	this->schema = schema;
+}
+
+void DatabaseConnector::connect()
+{
+	try{
+		this->driver = get_driver_instance();
+		string url = this->tcp + this->ip + ":" + this->port;
+		this->con = this->driver->connect(url, this->user, this->pwd);
+	}
+	catch (sql::SQLException& e){
+		cout << "# ERR: SQLException in " << __FILE__;
+	  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	  	cout << "# ERR: " << e.what();
+	  	cout << " (MySQL error code: " << e.getErrorCode();
+	  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+	  	throw;
+	}	
+}
+
+void DatabaseConnector::connect(const string& ip, const string& port, const string& user, const string& pwd)
+{
+	this->ip = ip;
+	this->port = port;
+	this->user = user;
+	this->pwd = pwd;
+	this->schema = schema;
+	try{
+		connect();
+	}
+	catch(...){
+		throw;
+	}
+}
+
+void DatabaseConnector::setSchema()
+{
+	try{
+		this->con->setSchema(this->schema);
+	}
+	catch(sql::SQLException& e){
+		cout << "# ERR: SQLException in " << __FILE__;
+	  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	  	cout << "# ERR: " << e.what();
+	  	cout << " (MySQL error code: " << e.getErrorCode();
+	  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+		throw;
+	}
+}
+
+void DatabaseConnector::setSchema(const string& schema)
+{
+	this->schema = schema;
+	try{
+		setSchema();
+	}
+	catch(...){
+		throw;
+	}
+}
+
+sql::Statement* DatabaseConnector::createStatement()
+{
+	sql::Statement* stmt = nullptr;
+	try{
+		stmt = this->con->createStatement();
+	}
+	catch(sql::SQLException& e){
+		cout << "# ERR: SQLException in " << __FILE__;
+	  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	  	cout << "# ERR: " << e.what();
+	  	cout << " (MySQL error code: " << e.getErrorCode();
+	  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+		throw;
+	}
+	return stmt;
+}
+
+sql::ResultSet* DatabaseConnector::executeQuery(sql::Statement* stmt, const string& query)
+{
+	sql::ResultSet* res = nullptr;
+	try{
+		res = stmt->executeQuery(query);
+	}
+	catch(sql::SQLException& e){
+		cout << "# ERR: SQLException in " << __FILE__;
+	  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+	  	cout << "# ERR: " << e.what();
+	  	cout << " (MySQL error code: " << e.getErrorCode();
+	  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+		throw;
+	}
+	return res;
+}
+
+sql::ResultSet* DatabaseConnector::createStatementAndExecuteQuery(const string& query)
+{
+	sql::ResultSet* res;
+	try{
+		sql::Statement* stmt = createStatement();
+		res = executeQuery(stmt, query);
+	}
+	catch(...){
+		throw;
+	}
+	return res;
+}
+
+
 
 int main(void)
 {
 	cout << endl;
-	cout << "Running 'SELECT 'Hello World!' AS _message'..." << endl;
+	cout << "Running 'SELECT * FROM testTable;'..." << endl;
 
 	try {
 	 	sql::Driver *driver;
@@ -29,16 +145,16 @@ int main(void)
 
 	  	/* Create a connection */
 	  	driver = get_driver_instance();
-	  	con = driver->connect("tcp://127.0.0.1:3306", "root", "root");
+	  	con = driver->connect("tcp://127.0.0.1:3306", "testUser", "pwd");
 	  	/* Connect to the MySQL test database */
 	  	con->setSchema("test");
 
 	  	stmt = con->createStatement();
-	  	res = stmt->executeQuery("SELECT 'Hello World!' AS _message"); // replace with your statement
+	  	res = stmt->executeQuery("SELECT * FROM testTable;");
 	  	while (res->next()) {
 	    	cout << "\t... MySQL replies: ";
 	    	/* Access column data by alias or column name */
-	    	cout << res->getString("_message") << endl;
+	    	cout << res->getString("text") << endl;
 	    	cout << "\t... MySQL says it again: ";
 	    	/* Access column fata by numeric offset, 1 is the first column */
 	    	cout << res->getString(1) << endl;
@@ -49,11 +165,11 @@ int main(void)
 
 		} 
 		catch (sql::SQLException &e) {
-	  	cout << "# ERR: SQLException in " << __FILE__;
-	  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
-	  	cout << "# ERR: " << e.what();
-	  	cout << " (MySQL error code: " << e.getErrorCode();
-	  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
+		  	cout << "# ERR: SQLException in " << __FILE__;
+		  	cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
+		  	cout << "# ERR: " << e.what();
+		  	cout << " (MySQL error code: " << e.getErrorCode();
+		  	cout << ", SQLState: " << e.getSQLState() << " )" << endl;
 	}
 
 	cout << endl;
---
+++ src/db.h
@@ -0,0 +1,44 @@
+/* Standard C++ includes */
+#include <stdlib.h>
+#include <iostream>
+
+/*
+  Include directly the different
+  headers from cppconn/ and mysql_driver.h + mysql_util.h
+  (and mysql_connection.h). This will reduce your build time!
+*/
+#include "mysql_connection.h"
+
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace std;
+
+class DatabaseConnector
+{
+public:
+	DatabaseConnector();
+	DatabaseConnector(const string& ip, const string& port, const string& user, const string& pwd);
+	DatabaseConnector(const string& ip, const string& port, const string& user, const string& pwd, const string& schema);
+
+	void connect();
+	void connect(const string& ip, const string& port, const string& user, const string& pwd);
+	void setSchema();
+	void setSchema(const string& schema);
+	sql::Statement* createStatement();
+	sql::ResultSet* executeQuery(sql::Statement* stmt, const string& query);
+	sql::ResultSet* createStatementAndExecuteQuery(const string& query);
+
+private:
+	static string tcp;
+	string ip;
+	string port;
+	string user;
+	string pwd;
+	string schema;
+
+	sql::Driver* driver;
+	sql::Connection* con;
+};
